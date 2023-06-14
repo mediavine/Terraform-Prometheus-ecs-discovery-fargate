af164148c447b145382a130504464bbeb82e1514
keimille <keith@mediavine.com>
2023-06-14 10:04:15 -0400

    Fix typo and add formatting

--- iam.tf
+++ iam.tf
@@ -1,19 +1,19 @@
 resource "aws_iam_role" "prometheus_role" {
-  name = "${var.environment}-${var.name_prefix}-role"
+  name               = "${var.environment}-${var.name_prefix}-role"
   assume_role_policy = aws_iam_policy.prom_read_write_policy.policy
 
   depends_on = [aws_iam_policy.prom_read_write_policy]
 }
 
 resource "aws_iam_policy" "prom_read_write_policy" {
-  name = "${var.environment}-${var.name_prefix}-ecs-prom-policy"
-  path = "/"
+  name        = "${var.environment}-${var.name_prefix}-ecs-prom-policy"
+  path        = "/"
   description = "Policy to allow ecs discovery of prometheus metrics and writes to AMP"
 
   policy = templatefile("${path.module}/policies/prometheus-amp.json", {
     account_id = data.aws_caller_identity.current.account_id
-    region = var.region
-    cluster = var.cluster_name
-    s3_bucket = var.s3_bucket
+    region     = var.region
+    cluster    = var.cluster_name
+    s3_bucket  = var.s3_bucket
   })
 }
--- prometheus-td.tf
+++ prometheus-td.tf
@@ -1,11 +1,11 @@
 resource "aws_ecs_task_definition" "prometheus-fargate" {
-  family                = "prometheus-ecs-fargate-discovery"
-  execution_role_arn = var.prom_execution_role_arn == '' ? aws_iam_role.prometheus_role.arn : var.prom_execution_role_arn
-  task_role_arn = var.prom_task_role_arn == '' ? aws_iam_role.prometheus_role.arn : var.prom_task_role_arn
+  family                   = "prometheus-ecs-fargate-discovery"
+  execution_role_arn       = var.prom_execution_role_arn == "" ? aws_iam_role.prometheus_role.arn : var.prom_execution_role_arn
+  task_role_arn            = var.prom_task_role_arn == "" ? aws_iam_role.prometheus_role.arn : var.prom_task_role_arn
   requires_compatibilities = ["FARGATE"]
-  network_mode = "awsvpc"
-  cpu = var.prom_cpu
-  memory = var.prom_memory
+  network_mode             = "awsvpc"
+  cpu                      = var.prom_cpu
+  memory                   = var.prom_memory
 
   volume {
     name = "ecs-discovery"
@@ -14,32 +14,32 @@ resource "aws_ecs_task_definition" "prometheus-fargate" {
   container_definitions = jsonencode([
     {
       image = "tkgregory/prometheus-ecs-discovery:latest",
-      name = "prometheus-ecs-discovery",
+      name  = "prometheus-ecs-discovery",
       logConfiguration = {
         logDriver = "awslogs",
-        options =  {
-          "awslogs-group": "/ecs/ecs-prometheus",
-          "awslogs-region": var.region,
-          "awslogs-stream-prefix": "prometheus-ecs-discovery"
+        options = {
+          "awslogs-group" : "/ecs/ecs-prometheus",
+          "awslogs-region" : var.region,
+          "awslogs-stream-prefix" : "prometheus-ecs-discovery"
         }
       },
-      command =  [
+      command = [
         "-config.cluster=${var.cluster_name}",
         "-config.write-to=/output/ecs_file_sd.yml"
       ],
       mountPoints = [
         {
           containerPath = "/output"
-          sourceVolume = "ecs-discovery"
+          sourceVolume  = "ecs-discovery"
         }
       ]
     },
     {
-      image            = "ghcr.io/bruceharrison1984/ecomm-prometheus:v2.29.1",
-      name             = "prometheus",
+      image = "ghcr.io/bruceharrison1984/ecomm-prometheus:v2.29.1",
+      name  = "prometheus",
       logConfiguration = {
         logDriver = "awslogs",
-        options   = {
+        options = {
           "awslogs-group" : "/ecs/ecs-prometheus",
           "awslogs-region" : var.region,
           "awslogs-stream-prefix" : "prometheus"
